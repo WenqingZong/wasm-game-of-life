0aa3e49fa8b13fd5749947160646e11abd671cea
Wenqing Zong <wenqing.zong98@gmail.com>
2024-10-27 01:37:02 +0000

    add fps

--- www/index.js
+++ www/index.js
@@ -20,6 +20,48 @@ const canvas = document.getElementById("game-of-life-canvas");
 
 const ctx = canvas.getContext('2d');
 
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        // Convert the delta time since the last frame render into a measure of frames per second.
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // Save only the latest 100 timings.
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the statistics.
+        this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+    }
+};
+
 // Draw a set of equally-spaced horizontal lines, and a set of equally-spaced vertical lines.
 const drawGrid = () => {
     ctx.beginPath();
@@ -114,8 +156,9 @@ canvas.addEventListener("click", event => {
 
 // On each iteration, JS draws the current universe to the <pre>, and then calls Universe::tick.
 const renderLoop = () => {
-    universe.tick();
+    fps.render();
 
+    universe.tick();
     drawGrid();
     drawCells();
 
@@ -124,16 +167,6 @@ const renderLoop = () => {
 
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
-
-const fps = new class {
-    constructor() {
-        this.fps = document.getElementById("fps");
-        this.frames = [];
-        this.lastFrameTimeStamp = performance.now();
-    }
-
-
-};
 
 // Start the first iteration.
 drawGrid();
